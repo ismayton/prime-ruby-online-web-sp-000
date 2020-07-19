ac92ffb8378cccf01f7eed9c625a778457411ada
Ian Mayton <ian.mayton@gmail.com>
2020-07-19 16:12:02 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,12 @@
 def prime?(integer)
-  range = (2..7).to_a 
-  range.each do |denominator|
-    if integer % denominator == 0 
-      false 
+  if integer < 2 
+      false
+  else
+    range = (2..7).to_a 
+    range.each do |denominator|
+      if integer % denominator == 0 
+        false 
+      end
     end
-  end
   true
 end
