4f39e0c84b2058736a03273409940bd8bfce4137
Ian Mayton <ian.mayton@gmail.com>
2020-07-19 16:44:38 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,13 @@
 require 'pry'
 
 def prime?(integer)
+  range = (2..7).to_a
   if integer < 2 
     return false
-  elsif integer == 4
-    return false
+    break
+  else
+    range.each do |denominator|
+      return false if integer % denominator == 0
   else
     return true
   end
