4bb6da26517d54b8a95a7d61f3879398a1517b7c
Ian Mayton <ian.mayton@gmail.com>
2020-07-19 16:59:26 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,16 +3,15 @@ require 'pry'
 def prime?(integer)
   if integer < 2 
     return false
-  end
-
-  range = (2..11).to_a
-  range.each do |denominator|
-    if integer > denominator
-      if integer % denominator == 0 
-      return false
+  else
+    range = (2..11).to_a
+    range.each do |denominator|
+      if integer > denominator && integer % denominator == 0 
+        return false
+      else
+        return true
       end
     end
   end
-  return true
 end
 
