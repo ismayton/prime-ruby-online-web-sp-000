92bb8d15ffc9e19ae1830b53d1d17e33bd1298d3
Ian Mayton <ian.mayton@gmail.com>
2020-07-19 17:07:09 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -4,8 +4,10 @@ def prime?(integer)
   else
     range = (2..100).to_a
     range.each do |denominator|
-      if integer > denominator && integer % denominator == 0 
-        return false
+      while integer < denominator
+        if integer % denominator == 0 
+          return false
+        end
       end
     end
   end
