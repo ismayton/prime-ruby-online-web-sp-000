505ff85e053e7ad5dabc9378b79ca4ef2d82642a
Ian Mayton <ian.mayton@gmail.com>
2020-07-19 16:09:49 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,13 +1,12 @@
 def prime?(integer)
   puts 'Testing'
   puts integer
-  if integer < 2 
-    puts '< 2, false'
-    false
-  end
   range = (2..7).to_a 
   range.each do |denominator|
-    if integer % denominator == 0 
+    if integer < 2 
+      puts '< 2, false'
+      false 
+    elsif integer % denominator == 0 
       puts 'found denominator, false'
       false 
     end
